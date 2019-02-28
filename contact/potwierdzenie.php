43c370914d4d31bba4fc1e353fc6364e4e25f5e0
Jacek Maciejak <jacekmaciejak@poczta.fm>
2019-02-28 18:46:38 +0100

    Added new files php with a code to sending an email

---
+++ contact/potwierdzenie.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="pl">
+
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+	<title>Portfolio</title>
+	<link href="https://fonts.googleapis.com/css?family=Berkshire+Swash&amp;subset=latin-ext" rel="stylesheet">
+	<link rel="stylesheet" href="style.css">
+
+</head>
+
+<body>
+	<header style="background-color=red" color="green">
+		<h1>Wiadomość została wysłana. Dziękujemy za kontakt.</h1>
+	</header>
+	<main>
+	</main>
+
+</body>
+
+</html>
