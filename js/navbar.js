4cfa14dd9fba194a7c4aeac9ced4281660bd9d69
Jacek Maciejak <jacekmaciejak@poczta.fm>
2020-06-24 17:39:57 +0200

    Navbar added

---
+++ js/navbar.js
@@ -0,0 +1,32 @@
+const navbarToggler = document.querySelector(".navbar__toggler");
+const navbarMenu = document.querySelector(".navbar ul");
+const navbarLinks = document.querySelectorAll(".navbar a");
+
+navbarToggler.addEventListener("click", navbarTogglerClick);
+
+function navbarTogglerClick() {
+  navbarToggler.classList.toggle("open-navbar-toggler");
+  navbarMenu.classList.toggle("open");
+}
+
+navbarLinks.forEach(elem => elem.addEventListener("click", navbarLinkClick));
+
+function navbarLinkClick() {
+  if (navbarMenu.classList.contains("open")) {
+    navbarToggler.click();
+  }
+}
+
+// Show & hide nav while scrolling
+let lastScrollTop = 0;
+
+const navbar = document.getElementById("navbar");
+window.addEventListener("scroll", function() {
+  const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+  if (scrollTop > lastScrollTop) {
+    navbar.style.top = "-80px";
+  } else {
+    navbar.style.top = "0";
+  }
+  lastScrollTop = scrollTop;
+});
